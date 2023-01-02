d84eeefd0473c7df056b0ec9381cb23f6664bedb
shahira-sadat <shahira.sadat1@gmail.com>
2023-01-02 22:44:25 +0430

    Add project data

---
+++ data/projects.js
@@ -0,0 +1,75 @@
+const projects = [
+  {
+    title: "Rental Cars",
+    description: "This is an app in wich you can reserve a car, create a car, delete a car and show all your reserved cars. When you run the app you will see the screen with all available cars. You can see their details and reserve cars. ",
+    imageUrl: "./images/project_images/car.png",
+    technologies: ["Ruby on Rails", "React", "Redux", "JavaScript", "Ruby", "Swagger"],
+    liveVersion: "https://639355566c9e9858f93bb1bb--bejewelled-cucurucho-3790b4.netlify.app/",
+    source: "https://github.com/shahira-sadat/final-capstone-project-frontend",
+  },
+
+  {
+    title: "Movie Show",
+    description: "This project is the Microverse's second Capstonetone project. Its a web application based on an external API tvmaze that shows list of movies which can be liked and commented. We selected this API that provides data about Movies.",
+    imageUrl: "./images/project_images/movie.png",
+    technologies: ["tvmaze API", "JavaScript", "Webpack", "CSS", "Html", "ES6 modules"],
+    liveVersion: "https://nimplay.github.io/api-webapp/dist/",
+    source: "https://github.com/nimplay/api-webapp",
+  },
+
+  {
+    title: "Bootcamp.af",
+    description: "Bootcamp.af - is a youtube based e-learning platform shortlisted as one of the top 50 semifinalists in Google Developer Student Club Solution Challange 2021.",
+    imageUrl: "./images/project_images/bootcamp.png",
+    technologies: ["React", "Redux", "Express.js", "Firebase", "Google Cloud APIs (Youtube API, Google Classroom API)"],
+    liveVersion: "https://bootcamp.af/",
+    source: "https://github.com/ahzia/Bootcamp",
+  },
+
+  {
+    title: "Budget Manager",
+    description: "A mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spent and on what.",
+    imageUrl: "./images/project_images/BudgetManager.png",
+    technologies: ["Ruby on Rails", "Ruby", "Devise", "JavaScript", "CSS", "Html", "Bootstrap"],
+    liveVersion: "https://github.com/shahira-sadat/Budget-app",
+    source: "https://github.com/shahira-sadat/Budget-app",
+  },
+
+  {
+    title: "Space Travelers' Hub",
+    description: "This is a web application that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.",
+    imageUrl: "./images/project_images/spacehub.png",
+    technologies: ["JavaScript", "React", "Redux", "CSS", "Html"],
+    liveVersion: "https://630733198d53a95fb699dec3--dreamy-blini-ee4660.netlify.app/",
+    source: "https://github.com/shahira-sadat/Space-Travelers-Hub",
+  },
+
+  {
+    title: "Math Magicians",
+    description: "In this project I am creating a Single Page App using React regarding basic Calculator and its functionalities.",
+    imageUrl: "./images/project_images/math.png",
+    technologies: ["React", "JavaScript", "CSS", "ES6 modules", "JSX"],
+    liveVersion: "https://62f4719741529b5c0ef9e4a1--verdant-griffin-6890d8.netlify.app/#/",
+    source: "https://github.com/shahira-sadat/MathMagicians",
+  },
+
+    {
+    title: "Leader Board",
+    description: "A single page web app that displays scores submitted by different players. All data is preserved thanks to the external Leaderboard API service.",
+    imageUrl: "./images/project_images/LeaderBoard.png",
+    technologies: ["HTML", "JavaScript", "CSS", "ES6 modules", "Webpack"],
+    liveVersion: "https://shahira-sadat.github.io/Leaderboard/dist/index.html",
+    source: "https://github.com/shahira-sadat/Leaderboard",
+  },
+
+    {
+    title: "Bookstore",
+    description: "A simple website to store your favorite books by category, a Microverse project Created with React.js.",
+    imageUrl: "./images/project_images/Bookstore.png",
+    technologies: ["React", "Redux","JavaScript", "CSS", "ES6 modules", "JSX"],
+    liveVersion: "https://bookstore-shahir.herokuapp.com/",
+    source: "https://github.com/shahira-sadat/Bookstore",
+  },
+];
+
+export default projects;
